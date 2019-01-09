801f0b2cbfaee40992b9fb5f05e325beb9b464c8
noemi310 <44778116+noemi310@users.noreply.github.com>
2019-01-09 09:06:46 -0600

    poner inactivo en nivel y la fecha de registro de alumno

--- src/java/Mantenimiento/MantenimientoNivel.java
+++ src/java/Mantenimiento/MantenimientoNivel.java
@@ -1,6 +1,4 @@
-
 package Mantenimiento;
-
 
 import Mantenimiento.JpaUtil;
 import Persistencia.Nivel;
@@ -9,25 +7,23 @@ import java.util.List;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
 
-
-
 public class MantenimientoNivel {
 
     public static void main(String[] args) {
-     
-     //   List<Cliente> listaCliente= man.consultarTodosUsuario();
-      //  Iterator it=listaCliente.iterator();
+
+        //   List<Cliente> listaCliente= man.consultarTodosUsuario();
+        //  Iterator it=listaCliente.iterator();
         //while(it.hasNext()){
-          //  Usuario u=(Usuario) it.next();
-          // System.out.println("usuario"+u.getNombre());
-            //System.out.println("correo"+u.getCorreo());
+        //  Usuario u=(Usuario) it.next();
+        // System.out.println("usuario"+u.getNombre());
+        //System.out.println("correo"+u.getCorreo());
 //            MantenimientoNivel mn=new MantenimientoNivel();
 //            String grado="primero";
 //            mn.consultarNivel(grado);
 //            System.out.println(mn);
-            
-        }
-         /* Usuario usuario = new Usuario();
+    }
+
+    /* Usuario usuario = new Usuario();
         usuario.setNombre("guillermo");
         usuario.setCorreo("prueba2");
   
@@ -36,116 +32,112 @@ public class MantenimientoNivel {
         man.guardarUsuario(usuario);
 
     }
-*/
-    public int guardarNivel(Nivel nivel){
-        EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
-        int flag=0;
+     */
+    public int guardarNivel(Nivel nivel) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        int flag = 0;
         em.getTransaction().begin();
-        try{
+        try {
             em.persist(nivel);
             em.getTransaction().commit();
-            flag=1;
-        }catch(Exception e){
+            flag = 1;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             em.getTransaction().rollback();
-            flag=0;
-        }finally{
+            flag = 0;
+        } finally {
             em.close();
         }
         return flag;
     }
-    
-    public Nivel consultarNivel(String grado){
-          EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
-          Nivel nivel=null;
-          em.getTransaction().begin();
-          try{
-             nivel=em.find(Nivel.class, grado);
-              em.getTransaction().commit();
-              
-          }catch(Exception e){
-              em.getTransaction().rollback();
-              
-          }finally{
-              em.close();
-          }
-         return nivel; 
+
+    public Nivel consultarNivel(String grado) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        Nivel nivel = null;
+        em.getTransaction().begin();
+        try {
+            nivel = em.find(Nivel.class, grado);
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+
+        } finally {
+            em.close();
+        }
+        return nivel;
     }
-    
-    public List<Nivel> consultarTodosNivel(){
-        List<Nivel> listaNivel=null;
-          EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
-          em.getTransaction().begin();
-          try{
-              Query query=em.createQuery("SELECT n FROM Nivel n where n.estado='activo'");
-              
-              em.getTransaction().commit();
-              listaNivel=query.getResultList();
-              System.out.println(listaNivel);
-              return listaNivel;
-              
-          }catch(Exception e){
-              em.getTransaction().rollback();
-             return null; 
-          }finally{
-              em.close();
-          }
-          
+
+    public List<Nivel> consultarTodosNivel() {
+        List<Nivel> listaNivel = null;
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        em.getTransaction().begin();
+        try {
+            Query query = em.createQuery("SELECT n FROM Nivel n where n.estado='activo'");
+
+            em.getTransaction().commit();
+            listaNivel = query.getResultList();
+            System.out.println(listaNivel);
+            return listaNivel;
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            return null;
+        } finally {
+            em.close();
+        }
+
     }
- public int ActualizarNivel(Nivel nivel){
-          EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
-          Nivel niv=null;
-          em.getTransaction().begin();
-         int flag=0;
-          try{
-              niv=em.find(Nivel.class, nivel.getGrado());
-             niv.setGrado(nivel.getGrado());
-             niv.setCodigoEscuela(nivel.getCodigoEscuela());
+
+    public int ActualizarNivel(Nivel nivel) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        Nivel niv = null;
+        em.getTransaction().begin();
+        int flag = 0;
+        try {
+            niv = em.find(Nivel.class, nivel.getGrado());
+            niv.setGrado(nivel.getGrado());
+            niv.setCodigoEscuela(nivel.getCodigoEscuela());
             niv.setEstado(nivel.getEstado());
             niv.setCampo(nivel.getCampo());
-            
-          
-                      
-              em.getTransaction().commit();
-               flag=1;
-          }catch(Exception e){
-            
-              em.getTransaction().rollback();
-            flag=0; 
-          }finally{
-              em.close();
-          }
-        return flag;
-       
- 
-          
-    
-   
-}    
- 
-  public int eliminarNivel(Nivel nivel){
-          EntityManager em=JpaUtil.getEntityManagerFactory().createEntityManager();
-          Nivel niv=null;
-          em.getTransaction().begin();
-         int flag=0;
-          try{
-              niv=em.find(Nivel.class, nivel.getGrado());
-            em.remove(niv);
-                      
-              em.getTransaction().commit();
-               flag=1;
-          }catch(Exception e){
-            
-              em.getTransaction().rollback();
-            flag=0; 
-          }finally{
-              em.close();
-          }
-        return flag;
-       
 
-}
-  
-  
- 
+            em.getTransaction().commit();
+            flag = 1;
+        } catch (Exception e) {
+
+            em.getTransaction().rollback();
+            flag = 0;
+        } finally {
+            em.close();
+        }
+        return flag;
+
+    }
+
+    public int eliminarNivel(Nivel nivel) {
+        EntityManager em = JpaUtil.getEntityManagerFactory().createEntityManager();
+        Nivel niv = null;
+        int flag = 0;
+        try {
+
+            em.getTransaction().begin();
+            niv = em.find(Nivel.class, nivel.getGrado());
+            niv.setGrado(nivel.getGrado());
+            niv.setCodigoEscuela(nivel.getCodigoEscuela());
+            niv.setEstado("inactivo");
+            niv.setCampo(nivel.getCampo());
+            em.getTransaction().commit();
+
+            flag = 1;
+        } catch (Exception e) {
+
+            em.getTransaction().rollback();
+            flag = 0;
+        } finally {
+            em.close();
+        }
+        return flag;
+
+    }
+
 }
