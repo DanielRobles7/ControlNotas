db55cff0dd8fe01e82c07f7f56a32ed89f43d582
Daniel Robles <daniel.robles@korinver.com>
2019-01-20 23:28:54 -0600

    ajuste en bean notas para filtrar alumnos

--- src/java/ManagedBean/BeanNotas.java
+++ src/java/ManagedBean/BeanNotas.java
@@ -12,6 +12,7 @@ import Persistencia.Alumnos;
 import Persistencia.Materias;
 import Persistencia.Notas;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -28,6 +29,8 @@ import javax.faces.context.FacesContext;
 public class BeanNotas {
 
     Notas notas = new Notas();
+    Materias materias = new Materias();
+    Alumnos alumnos = new Alumnos();
     private List<Notas> listNo = new ArrayList();
     private List<Materias> listM = new ArrayList();
     private List<Alumnos> listNie = new ArrayList();
@@ -35,6 +38,7 @@ public class BeanNotas {
     @PostConstruct
     public void init() {
 
+        BeanLoginAcceso beanLoginAcceso = new BeanLoginAcceso();
         MantenimientoMaterias manm = new MantenimientoMaterias();
         MantenimientoNotas man = new MantenimientoNotas();
         MantenimientoAlumnos ma = new MantenimientoAlumnos();
@@ -43,6 +47,45 @@ public class BeanNotas {
         listNie = ma.consultarTodosAlumnos();
         listNo = man.consultar();
         listM = manm.consultartodasMaterias();
+
+        int idLogueado = beanLoginAcceso.getUser_id();  //aca debemos traer el valor del alumno logueado
+        List<Notas> newList = new LinkedList<>();
+
+        for (int i = 0; i < listNo.size(); i++) {
+            if (listNo.get(i).getNie().getIdAcceso().getIdAcceso() == idLogueado) {           
+                // de notas
+                notas.setIdNota(listNo.get(i).getIdNota());
+                notas.setNota1(listNo.get(i).getNota1());
+                notas.setNota2(listNo.get(i).getNota2());
+                notas.setNota3(listNo.get(i).getNota3());
+                notas.setNota4(listNo.get(i).getNota4());
+                notas.setNota5(listNo.get(i).getNota5());
+                notas.setNota6(listNo.get(i).getNota6());
+                notas.setPromerio(listNo.get(i).getPromerio());
+                notas.setReposicion(listNo.get(i).getReposicion());
+                notas.setFinal1(listNo.get(i).getFinal1());
+
+                // de alumno
+                alumnos.setNombre(listNo.get(i).getNie().getNombre());
+                alumnos.setApellido(listNo.get(i).getNie().getApellido());
+                notas.setNie(alumnos);
+
+                // de materia
+                materias.setNombreMateria(listNo.get(i).getNombreMateria().getNombreMateria());
+                materias.setPonderacion1(listNo.get(i).getNombreMateria().getPonderacion1());
+                materias.setPonderacion2(listNo.get(i).getNombreMateria().getPonderacion2());
+                materias.setPonderacion3(listNo.get(i).getNombreMateria().getPonderacion3());
+                materias.setPonderacion4(listNo.get(i).getNombreMateria().getPonderacion4());
+                materias.setPonderacion5(listNo.get(i).getNombreMateria().getPonderacion5());
+                materias.setPonderacion6(listNo.get(i).getNombreMateria().getPonderacion6());
+                notas.setNombreMateria(materias);
+
+                newList.add(notas);
+            }
+        }
+        if (newList.size() > 0) {
+            listNo = newList;
+        }
 
     }
 
@@ -98,10 +141,10 @@ public class BeanNotas {
 //        man.guardar(notas);
         String adv = "";
         if (man.guardar(notas) == 1) {
-        listNo = man.consultar();
-        notas.setNie(new Alumnos());
-        notas.setNombreMateria(new Materias());
-        notas = new Notas();
+            listNo = man.consultar();
+            notas.setNie(new Alumnos());
+            notas.setNombreMateria(new Materias());
+            notas = new Notas();
             adv = "Guardado correctamente";
         } else {
             adv = "No se ha Guardado correctamente";
@@ -133,7 +176,7 @@ public class BeanNotas {
 //        man.eliminar(notas);
         String adv = "";
         if (man.eliminar(notas) == 1) {
-        listNo = man.consultar();
+            listNo = man.consultar();
             adv = "Se elimino correctamente";
         } else {
             adv = "No se ha podido eliminar";
@@ -145,38 +188,37 @@ public class BeanNotas {
     public void actualizar() {
         System.out.println("este es de actualizar" + notas);
         MantenimientoNotas man = new MantenimientoNotas();
-        MantenimientoMaterias mant= new MantenimientoMaterias();
-        double resultado1,resultado2,resultado3,resultado4,resultado5,resultado6,promedio,notafinal;
+        MantenimientoMaterias mant = new MantenimientoMaterias();
+        double resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, promedio, notafinal;
         Materias materias;
         materias = mant.consultarMateria(notas.getNombreMateria().getNombreMateria());
-        resultado1 = (notas.getNota1()*materias.getPonderacion1());
-        resultado2=(notas.getNota2()*materias.getPonderacion2());
-        resultado3= (notas.getNota3()*materias.getPonderacion3());
-        resultado4= (notas.getNota4()*materias.getPonderacion4());
-        resultado5= (notas.getNota5()*materias.getPonderacion5());
-        resultado6= (notas.getNota6()*materias.getPonderacion6());
-        promedio = (resultado1+resultado2+resultado3+resultado4+resultado5+resultado6)/100;
+        resultado1 = (notas.getNota1() * materias.getPonderacion1());
+        resultado2 = (notas.getNota2() * materias.getPonderacion2());
+        resultado3 = (notas.getNota3() * materias.getPonderacion3());
+        resultado4 = (notas.getNota4() * materias.getPonderacion4());
+        resultado5 = (notas.getNota5() * materias.getPonderacion5());
+        resultado6 = (notas.getNota6() * materias.getPonderacion6());
+        promedio = (resultado1 + resultado2 + resultado3 + resultado4 + resultado5 + resultado6) / 100;
         notas.setPromerio(promedio);
-        
-        if( promedio >= 6) {
+
+        if (promedio >= 6) {
             notas.setReposicion(0.00);
         }
-        
-        if ( notas.getReposicion()!=0){
-        notafinal= (promedio+notas.getReposicion())/2;  
-         notas.setFinal1(notafinal);
-        }else {
-         notafinal= promedio;
-          notas.setFinal1(notafinal); 
+
+        if (notas.getReposicion() != 0) {
+            notafinal = (promedio + notas.getReposicion()) / 2;
+            notas.setFinal1(notafinal);
+        } else {
+            notafinal = promedio;
+            notas.setFinal1(notafinal);
         }
-        
-          
-       System.out.println(man.Actualizar(notas));
+
+        System.out.println(man.Actualizar(notas));
 
         String adv;
         System.out.println("esta es actualizar" + notas);
         if (man.Actualizar(notas) == 1) {
-        listNo = man.consultar();
+            listNo = man.consultar();
             adv = "Se ha Actualizado Correctamente";
         } else {
             adv = "No se ha podido actualizar";
@@ -184,5 +226,5 @@ public class BeanNotas {
         FacesMessage msg = new FacesMessage(adv);
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-    
+
 }
